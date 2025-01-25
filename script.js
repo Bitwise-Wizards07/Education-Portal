3b060dc228e69b7449d5a6add4c524df82eb6e83
Tanishq7361 <tanishq7361@gmail.com>
2025-01-25 23:39:32 +0530

    added circle

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ logout.addEventListener("click", () => {
 document.querySelector(".thumbnail").addEventListener("click", () => {
     window.location.href = "index.html";
 });
+
+var getCol1=document.getElementById("circle1");
+var getCol2=document.getElementById("circle2");
+var getCol3=document.getElementById("circle3");
+var i=0;
+setInterval(()=>{
+    getCol1.style.background="conic-gradient(crimson 0deg,crimson "+i+"deg,white "+i+"deg,white)";
+    getCol2.style.background="conic-gradient(yellow 0deg,yellow "+i+"deg,white "+i+"deg,white)";
+    getCol3.style.background="conic-gradient(green 0deg,green "+i+"deg,white "+i+"deg,white)";
+    i++;
+    if(i==360){
+        clearInterval(interval);
+    }
+},10);
