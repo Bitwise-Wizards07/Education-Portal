55f50bb17ca19f086bf4e888e866b1237efccc17
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-02-11 19:16:04 +0530

    Updated Login Backend

--- Login/login.js
+++ Login/login.js
@@ -1,134 +1,3 @@
-// const inputs = document.querySelectorAll("input");
-// const name = document.querySelector(".name input");
-// const email = document.querySelector(".email input");
-// const password = document.querySelector(".password input");
-// const viewPass = document.querySelectorAll(".inp i");
-// const confirmPass = document.querySelector(".confirmpass input");
-// const submit = document.querySelector(".submit button");
-// const signupbtn = document.querySelector(".signup a");
-
-// const validateForm = (event) => {
-//     const isSignUp = submit.innerText === "Sign Up";
-
-//     if(name.value === "" || name.value === null) {
-//         alert("Please, Enter Your Name!!");
-//         name.focus();
-//         event.preventDefault();
-//         return false;
-//     } else if(email.value === "" || email.value === null) {
-//         alert("Please, Enter Your Email!!");
-//         email.focus();
-//         event.preventDefault();
-//         return false;
-//     } else if(password.value === "" || password.value === null) {
-//         alert("Please, Enter PassWord!!");
-//         password.focus();
-//         event.preventDefault();
-//         return false;
-//     } else if(password.value.includes(" ")) {
-//         alert("Password CANNOT Contain Spaces!");
-//         password.focus();
-//         event.preventDefault();
-//         return false;
-//     } else if(isSignUp) {
-//         if(confirmPass.value === "" || confirmPass.value === null) {
-//             alert("Please, Enter Confirm PassWord!!");
-//             confirmPass.focus();
-//             event.preventDefault();
-//             return false;
-//         } else if(confirmPass.value !== password.value) {
-//             alert("PassWord and Confirm PassWord Are NOT Same!!");
-//             confirmPass.focus();
-//             event.preventDefault();
-//             return false;
-//         }
-//     } else {
-//         return true;
-//     }
-// };
-
-// inputs.forEach(input => {
-//     input.addEventListener("focus", () => {
-//         input.parentElement.classList.add("focus");
-//     });
-//     input.addEventListener("blur", () => {
-//         input.parentElement.classList.remove("focus");
-//     });
-// });
-
-// viewPass.forEach(pass => {
-//     pass.addEventListener("click", () => {
-//         const input = pass.previousElementSibling; 
-        
-//         if (input.type === "password") {
-//             input.type = "text";
-//             pass.classList.remove("fa-eye");
-//             pass.classList.add("fa-eye-slash");
-//         } else {
-//             input.type = "password";
-//             pass.classList.remove("fa-eye-slash");
-//             pass.classList.add("fa-eye");
-//         }
-//     });
-// });
-
-// submit.addEventListener("click", (event) => {
-//     event.preventDefault();
-
-//     const isSignUp = submit.innerText === "Sign Up";
-
-//     if(validateForm(event)) {
-//         if(isSignUp) {
-//             let user = document.querySelector("#name").value;
-//             let mail = document.querySelector("#email").value;
-//             let pass = document.querySelector("#password").value;
-
-//             if(user && email && pass) {
-//                 localStorage.setItem("user", user.trim());
-//                 localStorage.setItem("mail", mail.trim());
-//                 localStorage.setItem("pass", pass.trim());
-//                 alert("Sign Up Successful!! Now Log In..");
-//                 // location.reload();
-//             } else {
-//                 validateForm(event);
-//             }
-//         } else {
-//             let user = document.querySelector("#name").value;
-//             let mail = document.querySelector("#email").value;
-//             let pass = document.querySelector("#password").value;
-
-//             if(user.trim() === localStorage.getItem("user") &&
-//                mail.trim() === localStorage.getItem("mail") &&
-//                pass.trim() === localStorage.getItem("pass")) {
-//                 localStorage.setItem("loggedIn", true);
-//                 window.location.href = "../index.html";
-//             } else {
-//                 alert("You Are Not Signed Up.. So Sign Up first.. OR Check Credentials again..");
-//             }
-//         }
-//     }
-// });
-
-// signupbtn.addEventListener("click", () => {
-//     if (submit.innerText === "Login") {
-//         submit.innerText = "Sign Up";
-//         document.querySelector(".heading h1").innerText = "Sign Up";
-//         document.querySelector(".signup p").innerText = "Already have an account? ";
-//         document.querySelector(".signup a").innerText = "Login";
-//         document.querySelector(".container").style.height = "85vh";
-//         document.querySelector(".container").style.width = "27vw";
-//         document.querySelector(".confirmpass").classList.remove("hide");
-//     } else {
-//         submit.innerText = "Login";
-//         document.querySelector(".heading h1").innerText = "Login";
-//         document.querySelector(".signup p").innerText = "Don't have an account? ";
-//         document.querySelector(".signup a").innerText = "Sign Up";
-//         document.querySelector(".container").style.height = "72vh";
-//         document.querySelector(".container").style.width = "25vw";
-//         document.querySelector(".confirmpass").classList.add("hide");
-//     }
-// });
-
 const inputs = document.querySelectorAll("input");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
@@ -138,7 +7,6 @@ const confirmPassInput = document.querySelector("#confirmpass");
 const submit = document.querySelector(".submit button");
 const signupBtn = document.querySelector(".signup a");
 
-// Validate form before submitting
 const validateForm = (event) => {
     const isSignUp = submit.innerText === "Sign Up";
 
@@ -178,7 +46,6 @@ const validateForm = (event) => {
     return true;
 };
 
-// Handle input field focus styles
 inputs.forEach(input => {
     input.addEventListener("focus", () => {
         input.parentElement.classList.add("focus");
@@ -188,7 +55,6 @@ inputs.forEach(input => {
     });
 });
 
-// Show/Hide Password
 viewPass.forEach(pass => {
     pass.addEventListener("click", () => {
         const input = pass.previousElementSibling;
@@ -205,7 +71,6 @@ viewPass.forEach(pass => {
     });
 });
 
-// Handle Login / Sign-Up Submission
 submit.addEventListener("click", (event) => {
     event.preventDefault();
     const isSignUp = submit.innerText === "Sign Up";
@@ -216,16 +81,15 @@ submit.addEventListener("click", (event) => {
         let pass = passwordInput.value.trim();
 
         if (isSignUp) {
-            // Store user credentials in localStorage
+            
             localStorage.setItem("user", user);
             localStorage.setItem("mail", mail);
             localStorage.setItem("pass", pass);
             alert("Sign Up Successful!! Now Log In..");
 
-            // Redirect to Login page
             switchToLogin();
         } else {
-            // Check if credentials match stored values
+            
             if (
                 user === localStorage.getItem("user") &&
                 mail === localStorage.getItem("mail") &&
@@ -240,8 +104,7 @@ submit.addEventListener("click", (event) => {
     }
 });
 
-// Switch to Login mode after Sign-Up
-function switchToLogin() {
+const switchToLogin = () => {
     submit.innerText = "Login";
     document.querySelector(".heading h1").innerText = "Login";
     document.querySelector(".signup p").innerText = "Don't have an account?";
@@ -249,9 +112,8 @@ function switchToLogin() {
     document.querySelector(".container").style.height = "72vh";
     document.querySelector(".container").style.width = "25vw";
     document.querySelector(".confirmpass").classList.add("hide");
-}
+};
 
-// Handle Sign-Up/Login Toggle
 signupBtn.addEventListener("click", () => {
     if (submit.innerText === "Login") {
         submit.innerText = "Sign Up";
@@ -265,4 +127,3 @@ signupBtn.addEventListener("click", () => {
         switchToLogin();
     }
 });
-
